4d7a703f94bddf01988f6f0fa92961440e57d42a
Jun <jun.w4ng@gmail.com>
2021-10-09 15:33:19 -0300

    Navbar agregado

---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,13 @@
+export const NavBar = () =>{
+    return (
+        <nav className = "NavBarItems">
+            <div className="logo">J | W</div>
+            <ul className="nav-links">
+                <li><a href="https://www.google.com">Home</a></li>
+                <li><a href="https://www.google.com">Tienda</a></li>
+                <li><a href="https://www.google.com">Nosotros</a></li>
+                <li><a href="https://www.google.com">Contacto</a></li>                
+            </ul>
+        </nav>
+    )
+}
