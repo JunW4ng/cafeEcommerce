05fc8acaab0f4691bb410147f07475c601772313
Jun <jun.w4ng@gmail.com>
2021-10-14 19:07:11 -0300

    Carrito en Navbar agregado y proximamente catalogo

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,3 +1,5 @@
+import { CartWidget } from "../cartwidget/CartWidget"
+
 export const NavBar = () =>{
     return (
         <nav className = "NavBarItems">
@@ -6,7 +8,8 @@ export const NavBar = () =>{
                 <li><a href="https://www.google.com">Home</a></li>
                 <li><a href="https://www.google.com">Tienda</a></li>
                 <li><a href="https://www.google.com">Nosotros</a></li>
-                <li><a href="https://www.google.com">Contacto</a></li>                
+                <li><a href="https://www.google.com">Contacto</a></li>
+                <li><a href="https://www.google.com"><CartWidget /></a></li>                
             </ul>
         </nav>
     )
