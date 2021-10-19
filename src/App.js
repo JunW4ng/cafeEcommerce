06896534dd69658601a2488a24a88b94fe08ff74
Jun <jun.w4ng@gmail.com>
2021-10-19 16:42:00 -0300

    Desafio Contador con Boton

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import './App.css';
 import { NavBar } from './components/navbar/NavBar.js';
 import './components/navbar/NavBar.css';
-import { useState } from 'react';
 import { ItemListContainer } from './components/itemlistcontainer/ItemListContainer';
+import { ItemCount } from './components/itemcount/ItemCount';
 
 
 function App() {
-
   return (
     
     <div className="App">
   
       <NavBar />
       <ItemListContainer greeting='HOLA MUNDO' />
+      <ItemCount stock='9' initial='1'/>
 
       <header className='App-header'>
       <h1>Proximamente mas cosas!</h1>
       </header>
-
-    </div>
+    </div>    
   )
 }
 
